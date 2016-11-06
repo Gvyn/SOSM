f301da907b5e6541e426f29745bf80990155b715
rybakm <Maciej Rybak@N7COMMANDCENTRE>
2016-11-06 17:53:13 +0100

    Sale and Order objects

---
+++ SOSM1/SOSM1/InterfaceToDataBaseOrderMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSM1
+{
+    class InterfaceToDataBaseOrderMethods
+    {
+        /// <summary>
+        /// Creates list of Orders from Baskets. Should be used by function finalizing
+        /// users' transaction.
+        /// </summary>
+        /// <param name="basketList">List of Basket object.</param>
+        /// <param name="sale">Sale which the orders are elements of. 
+        /// Should be already saved in database, because it is a foreign key.</param>
+        /// <returns>List of Order objects.</returns>
+        public static List<Order> BasketListToOrderList(List<Basket> basketList, Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SOSM1/SOSM1/InterfaceToDataBaseSaleMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSM1
+{
+    class InterfaceToDataBaseSaleMethods
+    {
+        /// <summary>
+        /// Finilaize users' transaction, saves new Sale_history object in
+        /// database and Orders objects accordingly to baskets from argument.
+        /// First it should save new Sale in database, then orders
+        /// with proper foreign keys.
+        /// Needed user info is in basketsList and should be consistent
+        /// through the list. This method should also delete any user baskets
+        /// in database.
+        /// </summary>
+        /// <param name="baskets">List of basket object</param>
+        /// <returns>True if purchase succeded, false otherwise.</returns>
+        public static bool CreateSale(List<Baskets> basketsList)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns list of users' purchases.
+        /// </summary>
+        /// <param name="user">User object.</param>
+        /// <returns>List of Sale object. Null if user has no purchases.</returns>
+        public static List<Sale> GetSalesHistory(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns list of orders which the sale consist of.
+        /// </summary>
+        /// <param name="sale">Sale object.</param>
+        /// <returns>List of Order objects.</returns>
+        public static List<Order> GetSaleOrders(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
