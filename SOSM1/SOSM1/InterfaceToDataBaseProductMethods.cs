37e6d59f1f4263c711b9f7af2de4f23a5fb9dad2
gvyn <abbb@interia.eu>
2016-11-06 15:37:55 +0100

    Blabla

---
+++ SOSM1/SOSM1/InterfaceToDataBaseProductMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSM1
+{
+    static class InterfaceToDataBaseProductMethods
+    {
+
+        /// <summary>
+        /// Creates Product data object from the product with specified name in the database.
+        /// </summary>
+        /// <param name="ProductName">Name of the product we want to get data from.</param>
+        /// <returns>Product data object if product exists, null otherwise.</returns>
+        public static Product GetProductData(string ProductName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if user with user name equal to ProductName exists in database and is Active or Created.
+        /// </summary>
+        /// <param name="ProductName">String with product name that we want to check.</param>
+        /// <returns>True if exists, false otherwise.</returns>
+        public static bool ProductExists(string ProductName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds new product to database created from the Product data object
+        /// </summary>
+        /// <param name="NewProduct">Product data object for creating new product.</param>
+        /// <returns>True if it could add, false otherwise.</returns>
+        public static bool AddUser(User NewProduct)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Changes the state of a specified product in database.
+        /// If user state is Active, sets him as Archival.
+        /// If user state is Created, sets him as Active.
+        /// If user state is Archival, does nothing.
+        /// </summary>
+        /// <param name="ProductName">Product name of the product we want to modify.</param>
+        /// <returns>True if success, false otherwise.</returns>
+        public static bool DeleteUser(string ProductName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SOSM1/SOSM1/InterfaceToDataBaseUserMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace SOSM1
+{
+    static class InterfaceToDataBaseUserMethods
+    {
+        /// <summary>
+        /// Checks if user with user name equal to UserName exists in database and is Active or Created.
+        /// </summary>
+        /// <param name="UserName">String with user name that we want to check.</param>
+        /// <returns>True if exists, false otherwise.</returns>
+        public static bool UserExists(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates User data object from the user with specified name in the database.
+        /// </summary>
+        /// <param name="UserName">Nameof the user we want to get data from.</param>
+        /// <returns>User data object if user exists, null otherwise.</returns>
+        public static User GetUserData(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Adds new user to database created from the User data object
+        /// </summary>
+        /// <param name="NewUser">User data object for creating new user.</param>
+        /// <returns>True if it could add, false otherwise.</returns>
+        public static bool AddUser(User NewUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Changes the state of a specified user in database.
+        /// If user state is Active, sets him as Archival.
+        /// If user state is Created, sets him as Active.
+        /// If user state is Archival, does nothing.
+        /// </summary>
+        /// <param name="UserName">User name of the user we want to modify.</param>
+        /// <returns>True if success, false otherwise.</returns>
+        public static bool DeleteUser(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Attempts to log in using specified Username and password.
+        /// Checks Active and Created users.
+        /// If the state is Created changes state to Active.
+        /// </summary>
+        /// <param name="Username">Username to be checked.</param>
+        /// <param name="Password">Password to be checked.</param>
+        /// <returns>True if succeded. False otherwise.</returns>
+        public static bool LogIn(string Username, SecureString Password)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
