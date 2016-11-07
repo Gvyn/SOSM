f8c219bb1754d4282a8d3a108d3abb8cc0e29499
rybakm <Maciej Rybak@N7COMMANDCENTRE>
2016-11-07 22:12:49 +0100

    Order class

--- SOSM1/SOSM1/Order.cs
+++ SOSM1/SOSM1/Order.cs
@@ -8,5 +8,89 @@ namespace SOSM1
 {
     public class Order
     {
+        public Order(int SaleID, Product OrderedProduct, decimal Amount, decimal Price)
+        {
+            this.OrderID = -1;
+            this.SaleID = SaleID;
+            this.OrderedProduct = OrderedProduct;
+            this.Amount = Amount;
+            this.Price = Price;
+        }
+
+        public Order(int SaleID, Basket BasketToOrder, decimal ActualPrice = -1)
+        {
+            this.OrderID = -1;
+            this.SaleID = SaleID;
+            this.OrderedProduct = BasketToOrder.ProductInBasket;
+            this.Amount = BasketToOrder.Amount;
+            if (ActualPrice == -1)
+                ActualPrice = BasketToOrder.ProductInBasket.Price;
+            this.Price = ActualPrice;
+        }
+
+        private int orderID;
+        public int OrderID
+        {
+            get
+            {
+                return orderID;
+            }
+            set
+            {
+                orderID = value;
+            }
+        }
+
+        private int saleID;
+        public int SaleID
+        {
+            get
+            {
+                return saleID;
+            }
+            set
+            {
+                saleID = value;
+            }
+        }
+
+        private Product orderedProduct;
+        public Product OrderedProduct
+        {
+            get
+            {
+                return orderedProduct;
+            }
+            set
+            {
+                orderedProduct = value;
+            }
+        }
+
+        private decimal amount;
+        public decimal Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+
+        private decimal price;
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
     }
 }
