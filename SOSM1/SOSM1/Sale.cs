b6df631f7ef9864be79177c75082b3f5112237aa
gvyn <abbb@interia.eu>
2016-11-07 22:04:45 +0100

    Sale

--- SOSM1/SOSM1/Sale.cs
+++ SOSM1/SOSM1/Sale.cs
@@ -8,5 +8,51 @@ namespace SOSM1
 {
     public class Sale
     {
+        public Sale(int UserID)
+        {
+            saleID = -1;
+            this.UserID = UserID;
+            Date = DateTime.Now;
+        }
+
+        private int saleID;
+        public int SaleID
+        {
+            get
+            {
+                return saleID;
+            }
+            set
+            {
+                saleID = value;
+            }
+        }
+
+        private int userID;
+        public int UserID
+        {
+            get
+            {
+                return userID;
+            }
+            set
+            {
+                userID = value;
+            }
+        }
+
+        private DateTime date;
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+
     }
 }
