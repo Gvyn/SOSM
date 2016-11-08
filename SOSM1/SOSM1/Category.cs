59669f48c1f0791e0de3aa48ef18b4cbe4e7499e
rybakm <Maciej Rybak@N7COMMANDCENTRE>
2016-11-08 17:38:31 +0100

    category class and methods

---
+++ SOSM1/SOSM1/Category.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSM1
+{
+    class Category
+    {
+        private int categoryID;
+        public int CategoryID
+        {
+            get
+            {
+                return categoryID;
+            }
+            set
+            {
+                CategoryID = value;
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException();
+                name = value;
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException();
+                description = value;
+            }
+        }
+    }
+}
---
+++ SOSM1/SOSM1/InterfaceToDataBaseCategoryMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSM1
+{
+    class InterfaceToDataBaseCategoryMethods
+    {
+        /// <summary>
+        /// Crates a brand new, ready for action Category. 
+        /// </summary>
+        /// <param name="Name">Name of Category, like 'Vegetables' or 'Elder scrolls'</param>
+        /// <param name="Description">Description of category.</param>
+        /// <returns>Category object.</returns>
+        public static Category CreateCategory(string Name, string Description)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes unwanted, surely made by mistake, Category.
+        /// </summary>
+        /// <param name="Name">Name of Category.</param>
+        /// <returns>True if operation succeded, false otherwise.</returns>
+        public static bool DeleteCategory(string Name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
