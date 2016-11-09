7ea32006431ec96cd5cc88b8becdd0b5603104d2
rybakm <Maciej Rybak@N7COMMANDCENTRE>
2016-11-09 19:52:49 +0100

    Other methods

--- SOSM1/SOSM1/InterfaceToDataBase/InterfaceToDataBaseOtherMethods.cs
+++ SOSM1/SOSM1/InterfaceToDataBase/InterfaceToDataBaseOtherMethods.cs
@@ -14,7 +14,38 @@ namespace SOSM1
         /// <returns>WelcomeMessage</returns>
         public static string GetWelcomeMessage()
         {
-            throw new NotImplementedException();
+            using (var context = new SOSMEntities())
+            {
+                var message = context.Other.First();
+                return message.Welcome_message;
+            }
+        }
+
+        /// <summary>
+        /// Sets Welcome Message in database.
+        /// </summary>
+        /// <param name="newMessage">A new message.</param>
+        /// <returns>True if succeded, false otherwise.</returns>
+        public static bool SetWelcomeMessage(string newMessage)
+        {
+            using (var context = new SOSMEntities())
+            {
+                var message = context.Other.FirstOrDefault();
+                if (message == null)
+                {
+                    Other dbOther = new Other();
+                    dbOther.Welcome_message = newMessage;
+                    context.Other.Add(dbOther);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    message.Welcome_message = newMessage;
+                    context.Entry(message).Property(e => e.Welcome_message).IsModified = true;
+                    context.SaveChanges();
+                }
+                return true;
+            }
         }
 
         /// <summary>
@@ -23,7 +54,38 @@ namespace SOSM1
         /// <returns>ContactInfo</returns>
         public static string GetContactInfo()
         {
-            throw new NotImplementedException();
+            using (var context = new SOSMEntities())
+            {
+                var message = context.Other.First();
+                return message.Contact_info;
+            }
+        }
+
+        /// <summary>
+        /// Sets Contact Info in database.
+        /// </summary>
+        /// <param name="newMessage">A new message.</param>
+        /// <returns>True if succeded, false otherwise.</returns>
+        public static bool SetContactInfo(string newContactInfo)
+        {
+            using (var context = new SOSMEntities())
+            {
+                var message = context.Other.FirstOrDefault();
+                if (message == null)
+                {
+                    Other dbOther = new Other();
+                    dbOther.Contact_info = newContactInfo;
+                    context.Other.Add(dbOther);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    message.Contact_info = newContactInfo;
+                    context.Entry(message).Property(e => e.Contact_info).IsModified = true;
+                    context.SaveChanges();
+                }
+                return true;
+            }
         }
     }
 }
