03ebbf4b62c0810a283a40e3fc0f20ff4a8a2417
rybakm <Maciej Rybak@N7COMMANDCENTRE>
2016-11-24 12:16:28 +0100

    jak ustawić szerokość labelLine według szerokości adminPanel? pytanie proste, które wymaga trudnej odpowiedzi

--- SOSM1/SOSM1/MainFormUserControls/AdminUserControl.Designer.cs
+++ SOSM1/SOSM1/MainFormUserControls/AdminUserControl.Designer.cs
@@ -28,10 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.lineLabel = new System.Windows.Forms.Label();
+            this.buttonProducts = new System.Windows.Forms.Button();
+            this.buttonUsers = new System.Windows.Forms.Button();
+            this.buttonSales = new System.Windows.Forms.Button();
+            this.buttonSettings = new System.Windows.Forms.Button();
+            this.adminControlPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lineLabel
+            // 
+            this.lineLabel.AutoSize = true;
+            this.lineLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lineLabel.Location = new System.Drawing.Point(20, 50);
+            this.lineLabel.MaximumSize = new System.Drawing.Size(700, 2);
+            this.lineLabel.MinimumSize = new System.Drawing.Size(700, 2);
+            this.lineLabel.Name = "lineLabel";
+            this.lineLabel.Size = new System.Drawing.Size(700, 2);
+            this.lineLabel.TabIndex = 0;
+            this.lineLabel.Text = "label1";
+            // 
+            // buttonProducts
+            // 
+            this.buttonProducts.Location = new System.Drawing.Point(20, 10);
+            this.buttonProducts.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.buttonProducts.MaximumSize = new System.Drawing.Size(100, 30);
+            this.buttonProducts.MinimumSize = new System.Drawing.Size(100, 30);
+            this.buttonProducts.Name = "buttonProducts";
+            this.buttonProducts.Size = new System.Drawing.Size(100, 30);
+            this.buttonProducts.TabIndex = 1;
+            this.buttonProducts.Text = "PRODUKTY";
+            this.buttonProducts.UseVisualStyleBackColor = true;
+            this.buttonProducts.Click += new System.EventHandler(this.buttonProducts_Click);
+            // 
+            // buttonUsers
+            // 
+            this.buttonUsers.Location = new System.Drawing.Point(143, 10);
+            this.buttonUsers.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.buttonUsers.MaximumSize = new System.Drawing.Size(100, 30);
+            this.buttonUsers.MinimumSize = new System.Drawing.Size(100, 30);
+            this.buttonUsers.Name = "buttonUsers";
+            this.buttonUsers.Size = new System.Drawing.Size(100, 30);
+            this.buttonUsers.TabIndex = 2;
+            this.buttonUsers.Text = "UŻYTKOWNICY";
+            this.buttonUsers.UseVisualStyleBackColor = true;
+            this.buttonUsers.Click += new System.EventHandler(this.buttonUsers_Click);
+            // 
+            // buttonSales
+            // 
+            this.buttonSales.Location = new System.Drawing.Point(266, 10);
+            this.buttonSales.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.buttonSales.MaximumSize = new System.Drawing.Size(100, 30);
+            this.buttonSales.MinimumSize = new System.Drawing.Size(100, 30);
+            this.buttonSales.Name = "buttonSales";
+            this.buttonSales.Size = new System.Drawing.Size(100, 30);
+            this.buttonSales.TabIndex = 3;
+            this.buttonSales.Text = "TRANSAKCJE";
+            this.buttonSales.UseVisualStyleBackColor = true;
+            this.buttonSales.Click += new System.EventHandler(this.buttonSales_Click);
+            // 
+            // buttonSettings
+            // 
+            this.buttonSettings.Location = new System.Drawing.Point(389, 10);
+            this.buttonSettings.Margin = new System.Windows.Forms.Padding(20, 3, 3, 3);
+            this.buttonSettings.MaximumSize = new System.Drawing.Size(100, 30);
+            this.buttonSettings.MinimumSize = new System.Drawing.Size(100, 30);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(100, 30);
+            this.buttonSettings.TabIndex = 4;
+            this.buttonSettings.Text = "USTAWIENIA";
+            this.buttonSettings.UseVisualStyleBackColor = true;
+            this.buttonSettings.Click += new System.EventHandler(this.buttonSettings_Click);
+            // 
+            // adminControlPanel
+            // 
+            this.adminControlPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.adminControlPanel.Location = new System.Drawing.Point(4, 55);
+            this.adminControlPanel.Name = "adminControlPanel";
+            this.adminControlPanel.Size = new System.Drawing.Size(744, 405);
+            this.adminControlPanel.TabIndex = 5;
+            // 
+            // AdminUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.adminControlPanel);
+            this.Controls.Add(this.buttonSettings);
+            this.Controls.Add(this.buttonSales);
+            this.Controls.Add(this.buttonUsers);
+            this.Controls.Add(this.buttonProducts);
+            this.Controls.Add(this.lineLabel);
+            this.Name = "AdminUserControl";
+            this.Size = new System.Drawing.Size(751, 463);
+            this.SizeChanged += new System.EventHandler(this.AdminUserControl_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lineLabel;
+        private System.Windows.Forms.Button buttonProducts;
+        private System.Windows.Forms.Button buttonUsers;
+        private System.Windows.Forms.Button buttonSales;
+        private System.Windows.Forms.Button buttonSettings;
+        private System.Windows.Forms.Panel adminControlPanel;
     }
 }
--- SOSM1/SOSM1/MainFormUserControls/AdminUserControl.cs
+++ SOSM1/SOSM1/MainFormUserControls/AdminUserControl.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using SOSM1.AdminControls;
+
 namespace SOSM1
 {
     public partial class AdminUserControl : UserControl
@@ -15,6 +17,50 @@ namespace SOSM1
         public AdminUserControl()
         {
             InitializeComponent();
+        }
+
+        private void AdminUserControl_SizeChanged(object sender, EventArgs e)
+        {
+            lineLabel.Width = this.Width - 20;
+            lineLabel.Invalidate();
+        }
+
+        private void SwapAdminControl(UserControl newAdminControl)
+        {
+            if (newAdminControl == null)
+                throw new ArgumentNullException();
+            if (adminControlPanel.Controls.Count == 1 && newAdminControl.GetType().Equals(adminControlPanel.Controls[0].GetType()))
+                return;
+            newAdminControl.Dock = DockStyle.Fill;
+
+            List<Control> ctrls = new List<Control>();
+            foreach (Control c in adminControlPanel.Controls)
+                ctrls.Add(c);
+            adminControlPanel.Controls.Clear();
+            foreach (Control c in ctrls)
+                c.Dispose();
+
+            adminControlPanel.Controls.Add(newAdminControl);
+        }
+
+        private void buttonProducts_Click(object sender, EventArgs e)
+        {
+            SwapAdminControl(new ProductsAdminControl());
+        }
+
+        private void buttonUsers_Click(object sender, EventArgs e)
+        {
+            SwapAdminControl(new UsersAdminControl());
+        }
+
+        private void buttonSales_Click(object sender, EventArgs e)
+        {
+            SwapAdminControl(new SalesAdminControl());
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            SwapAdminControl(new SettingsAdminControl());
         }
     }
 }
